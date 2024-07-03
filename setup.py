0bc81335c5f4644fd3f4eb525b191b50c25e6d7d
teekuningas <erkkahe@gmail.com>
2024-07-03 21:24:37 +0300

    Try including missing files to container

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="luontopeli",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'luontopeli=src.main:main',
